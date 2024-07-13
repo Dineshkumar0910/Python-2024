0b6953e49da4bbdf4e490ed137c7acfd3f37f048
Dineshkumar0910 <65652960+Dineshkumar0910@users.noreply.github.com>
2024-07-13 12:25:02 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,11 @@
+set1 = {23,3,64,12,67,12,32,6,212,12}
+print(set1)
+#print(set1[0]) - Not possible
+set1.add(500)
+print(set1)
+set1.remove(23)
+print(set1)
+set1.pop()
+print(set1)
+set1.update([62,23,74,12])
+print(set1)
